53eddf8bb402b584df2522a111f667f9905b13d8
Sartlyan <shadrinaspb@gmail.com>
2024-11-07 15:51:27 +0300

    Добавлена обработка исключений

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
-
 from tkinter import *
 import requests
 from tkinter import filedialog as fd
+from tkinter import messagebox as mb
 from tkinter import ttk
 
 def upload():
-    filepath = fd.askopenfilename()
-    if filepath:
-        files = {'file': open(filepath, 'rb')}
-        response = requests.post('https://file.io', files=files)
-        if response.status_code == 200:
-            link = response.json()['link']
-            entry.insert(0, link)
+    try:
+        filepath = fd.askopenfilename()
+        if filepath:
+            with open(filepath, 'rb') as f:
+                files = {'file': f}
+                response = requests.post('https://file.io', files=files)
+                response.raise_for_status()
+                download_link = response.json().get('link')
+                if download_link:
+                    entry.delete(0, END)
+                    entry.insert(0, download_link)
+                else:
+                    raise ValueError("Не удалось получить ссылку для скачивания")
+    except ValueError as ve:
+        mb.showerror("Ошибка", f"Произошла ошибка: {ve}")
+    except Exception as e:
+        mb.showerror("Ошибка", f"Произошла ошибка: {e}")
+
 
 window = Tk()
 window.title("Сохранение файлов в облаке")
-window.geometry(“400x200”)
+window.geometry("360x100")
 
-button = ttk.Button(text="Загрузить файл", command=upload)
-button.pack()
+upload_button = ttk.Button(text="Загрузить файл", command=upload)
+upload_button.pack()
 
 entry = ttk.Entry()
 entry.pack()
